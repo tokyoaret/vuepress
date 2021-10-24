f348868d97756d9c9f5057e54cc1c4a5001c6ea4
totkyoaret <myeongjae.lee@linecorp.com>
2021-10-24 16:17:52 +0900

    aa

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,5 +2,14 @@ module.exports = {
   title: 'Hello',
   description: 'Just playing around',
   base: '/vuepress/',  
-  dest: 'docs'
+  dest: 'docs',
+  locales: {
+    '/': {
+      lang: 'ja'
+    }
+  },
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ]
 }
